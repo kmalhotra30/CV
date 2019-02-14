feb99beb13116ec4176f403e3ece156533db1459
kmalhotra30 <kmalhotra30@users.noreply.github.com>
2019-02-14 01:57:34 +0100

    Assignment 1 , Question 1 files

---
+++ check_integrability.m
@@ -0,0 +1,55 @@
+function [ p, q, SE ] = check_integrability( normals )
+%CHECK_INTEGRABILITY check the surface gradient is acceptable
+%   normals: normal image
+%   p : df / dx
+%   q : df / dy
+%   SE : Squared Errors of the 2 second derivatives
+
+% initalization
+p = zeros(size(normals)); % Why is it 3d?
+q = zeros(size(normals)); % Why is it 3d?
+SE = zeros(size(normals)); % Why is it 3d?
+
+%Reinitializing the sizes - Need to check with TA
+
+[h,w,d] = size(normals);
+p = zeros(h,w);
+q = zeros(h,w);
+SE = zeros(h,w);
+
+
+% ========================================================================
+% YOUR CODE GOES HERE
+% Compute p and q, where
+% p measures value of df / dx
+% q measures value of df / dy
+p = normals(:,:,1) ./ normals(:,:,3); % Maybe it should be negative
+q = normals(:,:,2) ./ normals(:,:,3); % Maybe it should be negative
+
+
+% ========================================================================
+
+
+
+p(isnan(p)) = 0;
+q(isnan(q)) = 0;
+
+
+
+% ========================================================================
+% YOUR CODE GOES HERE
+% approximate second derivate by neighbor difference
+% and compute the Squared Errors SE of the 2 second derivatives SE
+
+dpdy = p ./ normals(:,:,3);
+dqdy = q ./ normals(:,:,3);
+SE = (dpdy - dqdy) .* (dpdy - dqdy);
+
+
+% ========================================================================
+
+
+
+
+end
+
---
+++ estimate_alb_nrm.m
@@ -0,0 +1,67 @@
+function [ albedo, normal ] = estimate_alb_nrm( image_stack, scriptV, shadow_trick)
+%COMPUTE_SURFACE_GRADIENT compute the gradient of the surface
+%   image_stack : the images of the desired surface stacked up on the 3rd
+%   dimension
+%   scriptV : matrix V (in the algorithm) of source and camera information
+%   shadow_trick: (true/false) whether or not to use shadow trick in solving
+%   	linear equations
+%   albedo : the surface albedo
+%   normal : the surface normal
+
+
+[h, w, ~] = size(image_stack);
+if nargin == 2
+    shadow_trick = true;
+end
+
+% create arrays for 
+%   albedo (1 channel)
+%   normal (3 channels)
+albedo = zeros(h, w, 1);
+normal = zeros(h, w, 3);
+
+% =========================================================================
+% YOUR CODE GOES HERE
+% for each point in the image array
+%   stack image values into a vector i
+%   construct the diagonal matrix scriptI
+%   solve scriptI * scriptV * g = scriptI * i to obtain g for this point
+%   albedo at this point is |g|
+%   normal at this point is g / |g|
+
+if shadow_trick == true
+    for row=1:h
+        for col=1:w
+           i = image_stack(row,col,:); % This is a vector
+           i = squeeze(i);
+           scriptI = diag(i,0); %I for shadow trick
+           A = scriptI * scriptV; % AX = B
+           B = scriptI * i;
+           warning('off','all')
+           g = linsolve(A,B); % Least square solution
+           albedo(row,col,1) = norm(g);
+           normal(row,col,:) = g ./ albedo(row,col,1);
+        end
+    end
+else
+  for row=1:h
+        for col=1:w
+           i = image_stack(row,col,:);
+           i = squeeze(i);
+           warning('off','all')
+           A = scriptV;
+           B = i;
+           g = linsolve(A,B);
+           albedo(row,col,1) = norm(g);
+           normal(row,col,:) = g ./ albedo(row,col,1);
+        end
+    end  
+end
+
+
+
+
+% =========================================================================
+
+end
+
