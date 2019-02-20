9817f96a146d0c10427dc8b659eed2e0790cd6cb
Arvid <Arvid.Lindstrom@gmail.com>
2019-02-20 18:28:54 +0100

    cleaned up the photometric file.. again :)

--- photometric_stereo.m
+++ photometric_stereo.m
@@ -96,13 +96,13 @@ show_model(albedo, height_map);
 
 
 
-%% Sphere Color
+%% Sphere/Monkey in color
 % 8. Using the load_syn_images function with the provided color channel 
 % argument, we load the image stack and scriptV matrix for each color
 % channel
-[image_stackRed, scriptV] = load_syn_images('./photometrics_images/SphereColor/', 1);
-[image_stackGreen, scriptV] = load_syn_images('./photometrics_images/SphereColor/', 2);
-[image_stackBlue, scriptV] = load_syn_images('./photometrics_images/SphereColor/', 3);
+[image_stackRed, scriptV] = load_syn_images('./photometrics_images/MonkeyColor/', 1);
+[image_stackGreen, scriptV] = load_syn_images('./photometrics_images/MonkeyColor/', 2);
+[image_stackBlue, scriptV] = load_syn_images('./photometrics_images/MonkeyColor/', 3);
 
 % 9. We use the red channel to extract the dimensions
 [h, w, n] = size(image_stackRed);
@@ -113,70 +113,26 @@ disp('Computing surface albedo and normal map...')
 [albedoB, normalsB] = estimate_alb_nrm(image_stackBlue, scriptV,true);
 
 
-
-
 %% integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
 disp('Integrability checking')
 % 10. These values pR, qR --> pB, qB are strictly speaking not used 
 % in our reconstructions but we at some point found it interesting to 
 % investigate the amount of integral errors per color channel
-[pR, qR, SER] = check_integrability(normalsR);
-[pG, qG, SEG] = check_integrability(normalsG);
-[pB, qB, SEB] = check_integrability(normalsB);
+% [pR, qR, SER] = check_integrability(normalsR);
+% [pG, qG, SEG] = check_integrability(normalsG);
+% [pB, qB, SEB] = check_integrability(normalsB);
 
-%% Combine normals (Non Naive solution)
 
-% 11. This matlab segment is our non-naive solution to combining normals 
-% from different color channels. We desire to retian the NaNs, should they 
-% appear, rather than simply setting them to 0, as such we initialize a new
-% tensor named normalComb of dimensions image height, image width and 
-% depth = 3. We then place into normalComb all the combined normals for
-% which at least one normal was not NaN (as caused by 0 albedos), followed 
-% by converting the summed normal into a unit vector on lines 160-161
+%% Combine normals and reconstruct (Naive Solution)
+%% Run this matlab cell to combine normals from color channels the naive way
 
-normalComb = zeros(size(normalsR));
-[h,w,d] = size(normalComb);
+% 11. Combine normals the naive way by simply adding them up, followed 
+% by converting them into unit vectors, NOTE: that for the naive solution
+% we may not combine the green channel using the monkey images as this 
+% will result in normals made entirely of NaN's. This is not an issue for 
+% our improved version in the next cell.
 
-% Combining normals and normalizing (ensuring unit length)
-
-for i=1:h
-    for j = 1:w
-        for k = 1:d
-            % Are all normals from each channel equal to NaN?
-            if isnan(normalsR(i,j,k)) && isnan(normalsG(i,j,k)) && isnan(normalsB(i,j,k))
-                normalComb(i,j,k) = NaN;
-            end
-            if ~isnan(normalsR(i,j,k)) %<-- what about normals from red?
-                normalComb(i,j,k) = normalComb(i,j,k) + normalsR(i,j,k);
-            end
-            if ~isnan(normalsG(i,j,k)) %..normals from green?
-                normalComb(i,j,k) = normalComb(i,j,k) + normalsG(i,j,k);
-            end
-            if ~isnan(normalsB(i,j,k)) %..normals from blue?
-                normalComb(i,j,k) = normalComb(i,j,k) + normalsB(i,j,k);
-            end
-        end
-        % Normalize normal-vector (turn into unit vector)
-        norms = squeeze(normalComb(i,j,:));
-        normalComb(i, j, :) = norms / norm(norms);
-    end
-end
-
-%%
-
-% 12. The variables pComb and qComb will be our derivatives resulting from 
-% using our improved normals in normalComb
-[pComb, qComb, SE] = check_integrability(normalComb);
-
-threshold = 0.005;
-SE(SE <= threshold) = NaN; % for good visualization
-fprintf('Number of outliers: %d\n\n', sum(sum(SE > threshold)));
-
-%% compute the surface height
-
-% 13. Combine normals the naive way by simply adding them up, followed 
-% by converting them into unit vectors
-normals = normalsR + normalsG + normalsB;
+normals = normalsR + normalsB;% + normalsG;
 
 for row=1:h
     for col=1:w
@@ -185,36 +141,91 @@ for row=1:h
     end
 end
          
-%% % 14. The albedo is 3 dimensional (3rd dimension for
-% colour channel)
 % Note: that for plotting the colored Monkey we may not add the green 
 % albedo as it will contain undefined values NaNs
 albedo = zeros(size(albedoR,1),size(albedoR,2),3);
 albedo(:,:,1) = albedoR;
-albedo(:,:,2) = albedoG;
+% albedo(:,:,2) = albedoG;
 albedo(:,:,3) = albedoB;
 imshow(albedo);
 
-% 15. The following line may be commented out if the user wished to see the 
-% naive combination of normals from different channels
-% normals = normalComb;
 
-% 16. In the following three lines we calculate the derivatives using 
+% 12. In the following three lines we calculate the derivatives using 
 % our naive normals. The user may comment out either of these lines 
 % depending on which results should be displayed
 [pNaive, qNaive, SE] = check_integrability(normals);
-height_mapNaive = construct_surface( pNaive, qNaive , 'average');
-% height_mapCombined = construct_surface( pComb, qComb , 'average');
 
-% 17. Likewise as stated above, these lines should also be commented out 
-% accordingly to see the model of interest, otherwise the last function 
-% call will be the one displayed
+% Naive Normals:
+height_mapNaive = construct_surface( pNaive, qNaive , 'average');
+
 show_results(albedo, normals, SE);
 show_model(albedo, height_mapNaive);
-% show_model(albedo, height_mapCombined);
 
 
-%% 18. Plot only normal-map as a single image
+
+
+%% Combine normals and reconstruct (Non Naive solution)
+
+% 13. This matlab segment is our non-naive solution to combining normals 
+% from different color channels. We desire to retain the NaNs, should they 
+% appear, rather than simply setting them to 0, as such we initialize a new
+% tensor named normals of dimensions image height, image width and 
+% depth = 3. We then place into normalComb all the combined normals for
+% which at least one normal was not NaN (as caused by 0 albedos), followed 
+% by converting the summed normal into a unit vector on lines 160-161
+
+normals = zeros(size(normalsR));
+[h,w,d] = size(normals);
+
+% Combining normals and normalizing (ensuring unit length)
+
+for i=1:h
+    for j = 1:w
+        for k = 1:d
+            % Are all normals from each channel equal to NaN?
+            if isnan(normalsR(i,j,k)) && isnan(normalsG(i,j,k)) && isnan(normalsB(i,j,k))
+                normals(i,j,k) = NaN;
+            end
+            if ~isnan(normalsR(i,j,k)) %<-- what about normals from red?
+                normals(i,j,k) = normals(i,j,k) + normalsR(i,j,k);
+            end
+            if ~isnan(normalsG(i,j,k)) %..normals from green?
+                normals(i,j,k) = normals(i,j,k) + normalsG(i,j,k);
+            end
+            if ~isnan(normalsB(i,j,k)) %..normals from blue?
+                normals(i,j,k) = normals(i,j,k) + normalsB(i,j,k);
+            end
+        end
+        % Normalize normal-vector (turn into unit vector)
+        norms = squeeze(normals(i,j,:));
+        normals(i, j, :) = norms / norm(norms);
+    end
+end
+
+% 14. The variables pComb and qComb will be our derivatives resulting from 
+% using our improved normals in normalComb
+[pComb, qComb, SE] = check_integrability(normals);
+
+threshold = 0.005;
+SE(SE <= threshold) = NaN; % for good visualization
+fprintf('Number of outliers: %d\n\n', sum(sum(SE > threshold)));
+
+% Note: that for plotting the colored Monkey we may not add the green 
+% albedo as it will contain undefined values NaNs
+albedo = zeros(size(albedoR,1),size(albedoR,2),3);
+albedo(:,:,1) = albedoR;
+% albedo(:,:,2) = albedoG;
+albedo(:,:,3) = albedoB;
+imshow(albedo);
+
+height_mapCombined = construct_surface( pComb, qComb , 'average');
+show_results(albedo, normals, SE);
+show_model(albedo, height_mapCombined);
+
+
+
+
+%% 15. Plot only normal-map as a single image
 
 % The following code snippet has been taken from the show_results() 
 % function to simplify the extraction of clear images of normal maps 
