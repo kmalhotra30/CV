b043d360e7030824fc5fbfdcaaa5a16e2afca219
Arvid <Arvid.Lindstrom@gmail.com>
2019-02-20 20:13:04 +0100

    pushed Arwins code with fix and the resulting images

--- 3/recoloring.m
+++ 3/recoloring.m
@@ -1,8 +1,9 @@
-function recoloring(org_img_path)
-org_img = imread(org_img_path);
-green_img = cat(3, zeros(length(org_img(:,1,:)), length(org_img(1,:,:)), 'uint8'), 255*ones(length(org_img(:,1,:)), length(org_img(1,:,:)), 'uint8'), zeros(length(org_img(:,1,:)), length(org_img(1,:,:)), 'uint8'));
-reconstructed_img = im2double(org_img) .* im2double(green_img);
-subplot(1,2,1), imshow(org_img), title("Original Image");
+function recoloring(shading_img_path, orig_path)
+shading_img = imread(shading_img_path);
+orig_img = imread(orig_path);
+green_img = cat(3, zeros(length(shading_img(:,1,:)), length(shading_img(1,:,:)), 'uint8'), 255*ones(length(shading_img(:,1,:)), length(shading_img(1,:,:)), 'uint8'), zeros(length(shading_img(:,1,:)), length(shading_img(1,:,:)), 'uint8'));
+reconstructed_img = im2double(shading_img) .* im2double(green_img);
+subplot(1,2,1), imshow(orig_img), title("Original Image");
 subplot(1,2,2), imshow(reconstructed_img), title("Reconstructed Image")
 
 end
