3fa0be3a4ebdd79450c5ff3fd34136885cdd82f6
Arvid <Arvid.Lindstrom@gmail.com>
2019-02-20 16:18:34 +0100

    cleaned up photometric file

--- photometric_stereo.m
+++ photometric_stereo.m
@@ -4,11 +4,17 @@ clear all
 clc
 %% 
 
+% 1. Doc: This segment of the code is used for exercises without colors 
+% nor faces. 
+
 disp('Part 1: Photometric Stereo')
 
 % obtain many images in a fixed view under different illumination
 disp('Loading images...')
-image_dir = './photometrics_images/MonkeyGray/';   % TODO: get the path of the script
+
+% 2. To load different images we change the folder name to the 
+% desired image folder
+image_dir = './photometrics_images/MonkeyGray/';  
 %image_ext = '*.png';
 
 [image_stack, scriptV] = load_syn_images(image_dir);
@@ -16,33 +22,37 @@ image_dir = './photometrics_images/MonkeyGray/';   % TODO: get the path of the s
 fprintf('Finish loading %d images.\n\n', n);
 
 % compute the surface gradient from the stack of imgs and light source mat
+% 3. To choose how many images to use, for example in question 4 of 
+% photometric stereo, we change the arguments in estimate_alb_nrm() to 
+% image_stack(:, :, low:upper) and scriptV(lower:upper, :)
 disp('Computing surface albedo and normal map...')
-[albedo, normals] = estimate_alb_nrm(image_stack(:, :, 1:50), scriptV(1:50, :), true);
+% The last argument is a flag which decided wether or not to use the 
+% shadow trick when estimating normals and albedos
+[albedo, normals] = estimate_alb_nrm(image_stack(:, :, :), scriptV(:, :), true);
 
-% Code for printing the normals
-%[h,w,d] = size(normals);
-%figure(1)
-%for i=1:h
-%    for j=1:w
-%        quiver3(i,j,0,normals(i,j,1),normals(i,j,2),normals(i,j,3))
-%    end
-%end
 
 %% integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
 disp('Integrability checking')
 [p, q, SE] = check_integrability(normals);
 
+% 4. As a threshold we arbitrarily chose 0.005 as our epsilon
 threshold = 0.005;
 SE(SE <= threshold) = NaN; % for good visualization
 fprintf('Number of outliers: %d\n\n', sum(sum(SE > threshold)));
 
 %% compute the surface height
+% 5. This function takes as last argument either "row", "column" or
+% "average"
 height_map = construct_surface( p, q ,'average');
 
 %% Display
 
-% show_results(albedo, normals, SE);
-% show_model(albedo, height_map);
+% 6. The following three lines have been commented out/in as necessary 
+% to obtain the desired plots
+show_results(albedo, normals, SE);
+show_model(albedo, height_map);
+
+% This call is useful to see just the albedo image
 imshow(albedo)
 
 
@@ -54,6 +64,10 @@ imshow(albedo)
 
 
 %% Face
+
+% 7. The first three segments up until %%Sphere Color work analogously as 
+% the code above
+
 [image_stack, scriptV] = load_face_images('./photometrics_images/yaleB02/');
 [h, w, n] = size(image_stack);
 fprintf('Finish loading %d images.\n\n', n);
@@ -83,15 +97,14 @@ show_model(albedo, height_map);
 
 
 %% Sphere Color
+% 8. Using the load_syn_images function with the provided color channel 
+% argument, we load the image stack and scriptV matrix for each color
+% channel
 [image_stackRed, scriptV] = load_syn_images('./photometrics_images/MonkeyColor/', 1);
 [image_stackGreen, scriptV] = load_syn_images('./photometrics_images/MonkeyColor/', 2);
 [image_stackBlue, scriptV] = load_syn_images('./photometrics_images/MonkeyColor/', 3);
-% imshow(image_stackRed(:, :, 1))
-% imshow(image_stackGreen(:, :, 1))
-% imshow(image_stackBlue(:, :, 1))
 
-
-size(image_stackRed)
+% 9. We use the red channel to extract the dimensions
 [h, w, n] = size(image_stackRed);
 fprintf('Finish loading %d images.\n\n', n);
 disp('Computing surface albedo and normal map...')
@@ -104,11 +117,22 @@ disp('Computing surface albedo and normal map...')
 
 %% integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
 disp('Integrability checking')
+% 10. These values pR, qR --> pB, qB are strictly speaking not used 
+% in our reconstructions but we at some point found it interesting to 
+% investigate the amount of integral errors per color channel
 [pR, qR, SER] = check_integrability(normalsR);
 [pG, qG, SEG] = check_integrability(normalsG);
 [pB, qB, SEB] = check_integrability(normalsB);
 
-%% Combine normals
+%% Combine normals (Non Naive solution)
+
+% 11. This matlab segment is our non-naive solution to combining normals 
+% from different color channels. We desire to retian the NaNs, should they 
+% appear, rather than simply setting them to 0, as such we initialize a new
+% tensor named normalComb of dimensions image height, image width and 
+% depth = 3. We then place into normalComb all the combined normals for
+% which at least one normal was not NaN (as caused by 0 albedos), followed 
+% by converting the summed normal into a unit vector on lines 160-161
 
 normalComb = zeros(size(normalsR));
 [h,w,d] = size(normalComb);
@@ -117,19 +141,18 @@ normalComb = zeros(size(normalsR));
 
 for i=1:h
     for j = 1:w
-%         dummy = [0, 0, 0];
         for k = 1:d
-            
+            % Are all normals from each channel equal to NaN?
             if isnan(normalsR(i,j,k)) && isnan(normalsG(i,j,k)) && isnan(normalsB(i,j,k))
                 normalComb(i,j,k) = NaN;
             end
-            if ~isnan(normalsR(i,j,k))
+            if ~isnan(normalsR(i,j,k)) %<-- what about normals from red?
                 normalComb(i,j,k) = normalComb(i,j,k) + normalsR(i,j,k);
             end
-            if ~isnan(normalsG(i,j,k))
+            if ~isnan(normalsG(i,j,k)) %..normals from green?
                 normalComb(i,j,k) = normalComb(i,j,k) + normalsG(i,j,k);
             end
-            if ~isnan(normalsB(i,j,k))
+            if ~isnan(normalsB(i,j,k)) %..normals from blue?
                 normalComb(i,j,k) = normalComb(i,j,k) + normalsB(i,j,k);
             end
         end
@@ -141,8 +164,9 @@ end
 
 %%
 
+% 12. The variables pComb and qComb will be our derivatives resulting from 
+% using our improved normals in normalComb
 [pComb, qComb, SE] = check_integrability(normalComb);
-
 
 threshold = 0.005;
 SE(SE <= threshold) = NaN; % for good visualization
@@ -150,8 +174,9 @@ fprintf('Number of outliers: %d\n\n', sum(sum(SE > threshold)));
 
 %% compute the surface height
 
-% Combine normals the naive way
-normals = normalsR + normalsB;% + normalsB;
+% 13. Combine normals the naive way by simply adding them up, followed 
+% by converting them into unit vectors
+normals = normalsR + normalsG + normalsB;
 
 for row=1:h
     for col=1:w
@@ -160,17 +185,34 @@ for row=1:h
     end
 end
          
+% 14. The albedos used for plotting are simply added together
+% Note: that for plotting the colored Monkey we may not add the green 
+% albedo as it will contain undefined values NaNs
+albedo = albedoR + albedoG + albedoB;
 
-albedo = albedoR + albedoB;% + albedoG;
+% 15. The following line may be commented out if the user wished to see the 
+% naive combination of normals from different channels
 normals = normalComb;
 
-% [pNaive, qNaive, SE] = check_integrability(normals);
-% height_mapNaive = construct_surface( pNaive, qNaive , 'average');
+% 16. In the following three lines we calculate the derivatives using 
+% our naive normals. The user may comment out either of these lines 
+% depending on which results should be displayed
+[pNaive, qNaive, SE] = check_integrability(normals);
+height_mapNaive = construct_surface( pNaive, qNaive , 'average');
 height_mapCombined = construct_surface( pComb, qComb , 'average');
 
-% show_results(albedo, normals, SE);
-% show_model(albedo, height_mapNaive);
+% 17. Likewise as stated above, these lines should also be commented out 
+% accordingly to see the model of interest, otherwise the last function 
+% call will be the one displayed
+show_results(albedo, normals, SE);
+show_model(albedo, height_mapNaive);
 show_model(albedo, height_mapCombined);
+
+%% 18. Plot only normal-map as a single image
+
+% The following code snippet has been taken from the show_results() 
+% function to simplify the extraction of clear images of normal maps 
+% rather than plotting them as subfigures
 
 % PLOT NORMALS:
 minN = min(min(normals, [], 1), [], 2);
